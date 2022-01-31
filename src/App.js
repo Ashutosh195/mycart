a4c36124a6629dc4dfcea6d6529177824c21c414
Ashutosh <ashtripathi124@gmail.com>
2022-01-31 15:37:38 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <div className="App">
+           {/* <Home /> */}
         <Route path="/" exact>
           <Home />
         </Route>
